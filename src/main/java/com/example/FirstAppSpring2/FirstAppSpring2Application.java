128d795d254c5046f3c225607cc411aae1a806d1
unknown <Komar9001@wp.pl>
2025-05-28 23:16:49 +0200

    Zadanie 8

---
+++ src/main/java/com/example/FirstAppSpring2/FirstAppSpring2Application.java
@@ -0,0 +1,11 @@
+package com.example.FirstAppSpring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAppSpring2Application {
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAppSpring2Application.class, args);
+	}
+}
---
+++ src/main/java/com/example/FirstAppSpring2/service/VehicleService.java
@@ -0,0 +1,27 @@
+package com.example.FirstAppSpring2.service;
+
+
+import com.example.FirstAppSpring2.model.Vehicle;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface VehicleService {
+
+    List<Vehicle> findAll();
+
+    List<Vehicle> findAllActive();
+
+    Optional<Vehicle> findById(String id);
+
+    Vehicle save(Vehicle vehicle);
+
+    List<Vehicle> findAvailableVehicles();
+
+    List<Vehicle> findRentedVehicles();
+
+    boolean isAvailable(String vehicleId);
+
+    //"it should be soft delete"
+    void deleteById(String id);
+}
