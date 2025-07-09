f67d246cf6a9e8c26d1b3d21ff66e3fc741a1342
unknown <Komar9001@wp.pl>
2025-07-09 19:23:16 +0200

    moving

---
+++ FirstAppJdbc_Json_Hibernate/src/main/java/com/example/models/User.java
@@ -0,0 +1,25 @@
+package com.example.models;
+
+import lombok.*;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @Column(nullable = false, unique = true)
+    private String id;
+
+    @Column(nullable = false, unique = true)
+    private String login;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
